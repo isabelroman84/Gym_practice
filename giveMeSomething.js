d1bec807d709472cbc0eff5bfc2bf9bff9ea147c
isabelroman84 <isabelcristinaroman@gmail.com>
2023-07-28 21:46:56 -0500

    feat: función que añade una palabra al inicio de un str

---
+++ giveMeSomething.js
@@ -0,0 +1,7 @@
+/* Write a function that returns the string "something" joined with a space " " and the given argument a. */
+
+const giveMeSomething = str => `Something ${str}`
+
+console.log(giveMeSomething("is better than nothing"))
+console.log(giveMeSomething("Bob Jane"))
+console.log(giveMeSomething("something"))
