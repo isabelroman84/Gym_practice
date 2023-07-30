32f20c06ecdecab13df5f0362ddf969d05dad2ff
isabelroman84 <isabelcristinaroman@gmail.com>
2023-07-29 19:23:04 -0500

    feat: función que retorna un array en orden inverso

---
+++ reverse3.js
@@ -0,0 +1,9 @@
+/* Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}. */
+
+function reverse3 (nums) {
+    return nums.reverse()
+}
+
+console.log(reverse3([1, 2, 3]))
+console.log(reverse3([5, 11, 9]))
+console.log(reverse3([7, 0, 0]))
